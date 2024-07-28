a26e056a18a07972ddd14b0f4209fc618802c914
zonasatu bae <zonasatu886@gmail.com>
2024-07-28 14:26:07 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("rodo","rd")
+        ERC721("podo","pd")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
